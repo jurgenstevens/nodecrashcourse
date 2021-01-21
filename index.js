a82f6e342c70de10a6dffe0ebe6fd47cb5d6b27c
Jurgen <jurgen.stevens@yahoo.com>
2021-01-20 22:25:05 -0600

    Deleted node_modules and completed person.js.

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 // 06: Run 'node index.js' in the terminal to see the text in the string below.
-console.log('Hello from Node.js')
+console.log('Hello from Node.js') 
+// 07: Create a module/file to import here. Switch to person.js for 08.
