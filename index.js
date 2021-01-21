d0d83cb13bbb94bd531eff29a83ebc785ff3a9cf
Jurgen <jurgen.stevens@yahoo.com>
2021-01-20 23:57:55 -0600

    Created a Person class w/ greeting function.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-// 06: Run 'node index.js' in the terminal to see the text in the string below.
-console.log('Hello from Node.js') 
-// 07: Create a module/file to import here. Switch to person.js for 08.
+// S06: Run 'node index.js' in the terminal to see the text in the string below.
+// console.log('Hello from Node.js') 
+// S07: Create a module/file to import here. Switch to person.js for S08.
+// S10: Switched from person.js to import person object from person.js
+// S10: const person = require('./person')
+
+// S11: Commented below, you can console.log whatever you want to see from the object, the entire obj, the name or the age. Switch to person.js for S12.
+// S12: console.log(person) // { name: 'John Doe', age: 30 }
+// S12: console.log(person.name) // John Doe
+// S12: console.log(person.age) // 30
--- person.js
+++ person.js
@@ -1,6 +1,19 @@
-const person = {
-    name: 'John Doe',
-    age: 30
+// S08: Create an object called person that will have a name and age.
+// const person = {
+//     name: 'John Doe',
+//     age: 30
+// }
+// S12: Create a class called Person and give it a greeting function using name and age. THEN switch to index.js for S13.
+class Person {
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+
+    greeting() {
+        console.log(`My name is ${this.name} and I am ${this.age}`)
+    }
 }
 
-module.exports = person
+// 09: To use the object elsewhere, export the object's name using module.exports. Switch to index.js for S10
+module.exports = Person
