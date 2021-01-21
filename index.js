4b084b01c3f57c029dc93199296845283f0b6541
Jurgen <jurgen.stevens@yahoo.com>
2021-01-21 00:14:58 -0600

    Created a perfectly working class w/ function  when concatinated.

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 // S06: Run 'node index.js' in the terminal to see the text in the string below.
 // console.log('Hello from Node.js') 
 // S07: Create a module/file to import here. Switch to person.js for S08.
-// S10: Switched from person.js to import person object from person.js
+// S10: Import person object from person.js
 // S10: const person = require('./person')
+// S13: Import Person class from person.js
+const Person = require('./person')
+
 
 // S11: Commented below, you can console.log whatever you want to see from the object, the entire obj, the name or the age. Switch to person.js for S12.
-// S12: console.log(person) // { name: 'John Doe', age: 30 }
-// S12: console.log(person.name) // John Doe
-// S12: console.log(person.age) // 30
+// S11: console.log(person) // { name: 'John Doe', age: 30 }
+// S11: console.log(person.name) // John Doe
+// S11: console.log(person.age) // 30
+
+// S14: Create a new person with arguments passed through the parameters
+const person1 = new Person('John Doe', 30);
+
+// S15: Then concatinate the greeting function with the new person you created.
+person1.greeting();
